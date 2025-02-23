c6c8138cce3d6523c86e59c204d7933d748807e1
VorCoda <jetik.yt345@gmail.com>
2025-02-23 16:27:14 +0500

    first commit

---
+++ src/main/java/com/example/Sinara/JavaSinaraApplication.java
@@ -0,0 +1,13 @@
+package com.example.Sinara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSinaraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSinaraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Sinara/JavaSinaraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Sinara;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSinaraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
