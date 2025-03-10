1300baaabe35d5eb058e42a26269202c71791680
VorCoda <jetik.yt345@gmail.com>
2025-03-10 10:18:09 +0500

    3_DI Task

---
+++ src/main/java/com/example/Sinara/DInjExmp.java
@@ -0,0 +1,10 @@
+package com.example.Sinara;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public interface DInjExmp {
+
+    void exmpPrint();
+}
+
---
+++ src/main/java/com/example/Sinara/SetterInj.java
@@ -0,0 +1,20 @@
+package com.example.Sinara;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SetterInj {
+
+    private DInjExmp dInjExmp;
+
+    @Autowired
+    public void setdInjExmp(@Qualifier("A") DInjExmp dInjExmp){
+        this.dInjExmp = dInjExmp;
+    }
+
+    public void print(){
+        dInjExmp.exmpPrint();
+    }
+}
